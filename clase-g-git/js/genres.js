e802ffee084efd3ee68a6ead63e5292b00646c98
marcoswilson1 <mwilson@udesa.edu.ar>
2021-11-17 15:09:36 -0300

    Pagina de generosjs

---
+++ clase-g-git/js/genres.js
@@ -0,0 +1,34 @@
+window.addEventListener("load",function(){
+fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=38d8aa65e8dac91d5716d80acccd64eb&language=en-US`)
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+    .then(function(datos){
+        console.log(datos);
+      let generospeliculas = document.querySelector('.generospeliculas')
+    datos.genres.forEach(datos => {generospeliculas.innerHTML +=`<a  href="../html/detail-genres.html" class="titulogenero">${datos.name}</a>`
+        
+    });        
+    
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+
+
+    fetch(`https://api.themoviedb.org/3/genre/tv/list?api_key=38d8aa65e8dac91d5716d80acccd64eb&language=en-US`)
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+    .then(function(datos){
+        console.log(datos);
+      let generosseries = document.querySelector('.generosseries')
+    datos.genres.forEach(datos => {generosseries.innerHTML +=`<a  href="../html/detail-genres.html" class="titulogenero">${datos.name}</a>`
+        
+    });        
+    
+    })
+    .catch(function(error){
+        console.log(error);
+    })
+})
