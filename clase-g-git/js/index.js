c302842d8b47b643e010eb2e6928cc2f01b7ed85
JeroPruneda-g <jprunedapaz@udesa.edu.ar-g>
2021-11-19 17:43:23 -0300

    ultimos detalles

--- clase-g-git/js/index.js
+++ clase-g-git/js/index.js
@@ -16,7 +16,7 @@ fetch(urlSeries)
         //Paso 2 bsucar los datos y actualizar el paso1
         for(let i=0; i<6; i++){
             elementosLista += `<article class="art-series">
-                                    <a class="peli" href="./detail-series.html${info[i].id}" > 
+                                    <a class="peli" href="./detail-series.html?id=${info[i].id}" > 
                                         <img src=https://image.tmdb.org/t/p/w154/${info[i].poster_path} alt="${info[i].name}">
                                         <p class="titulopeli">${info[i].name}</p>
                                         <p class="fecha">${info[i].first_air_date}</p>
@@ -51,7 +51,7 @@ fetch(urlPopulares)
         //Paso 2 bsucar los datos y actualizar el paso1
         for(let i=0; i<6; i++){
             elementosLista += `<article class="art-peli">
-                                    <a class="peli" href="./detail-movies.html${info[i].id}" > 
+                                    <a class="peli" href="./detail-movies.html?id=${info[i].id}" > 
                                         <img src=https://image.tmdb.org/t/p/w154/${info[i].poster_path} alt="${info[i].title}">
                                         <p class="titulopeli">${info[i].title}</p>
                                         <p class="fecha">${info[i].release_date}</p>
@@ -86,7 +86,7 @@ fetch(url)
         //Paso 2 bsucar los datos y actualizar el paso1
         for(let i=0; i<6; i++){
             elementosLista += `<article class="art-visto">
-            <a class="peli" href="./detail-movies.html${info[i].id}" > 
+            <a class="peli" href="./detail-movies.html?id=${info[i].id}" > 
                 <img src=https://image.tmdb.org/t/p/w154/${info[i].poster_path} alt="${info[i].title}">
                 <p class="titulopeli">${info[i].title}</p>
                 <p class="fecha">${info[i].release_date}</p>
