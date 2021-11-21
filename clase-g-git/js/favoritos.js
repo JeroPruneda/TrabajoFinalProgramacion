bea0cca69d8fa0f0e66fcd8a2061b9ecddf25647
JeroPruneda-g <jprunedapaz@udesa.edu.ar-g>
2021-11-21 14:17:48 -0300

    Ultmios cambios

--- clase-g-git/js/favoritos.js
+++ clase-g-git/js/favoritos.js
@@ -1,4 +1,4 @@
- //Peliculas favoritos
+ /* //Peliculas favoritos
 //Recupero storage
 let recuperoStorage = localStorage.getItem('favoritos');
 
@@ -69,8 +69,58 @@ function buscarYMostarFavoritos(id){
             console.log(error);
         })
 
+} */
+
+//Recupero storage
+let recuperoStorage = localStorage.getItem('peliculasFav');
+//y transformar de json en array
+let peliculasFav = JSON.parse(recuperoStorage);
+console.log(peliculasFav); 
+
+
+//Capturar el contenedor de los elementos a mostar
+let section = document.querySelector('.primer-contenedor');
+let personajesFavoritos = '';
+
+
+//Si el storage está vacío indicamos al usuario que no hay favoritos seleccionados.
+if(peliculasFav == null || peliculasFav.length == 0){
+    section.innerHTML='<p class="seleccionados">No hay favoritos seleccionados</p>'
+} else {
+    //for para recorrer el array.
+    for (let i=0; i<peliculasFav.length; i++){
+        buscarYMostarFavoritos(peliculasFav[i]);
+    }
+
 }
 
+
+function buscarYMostarFavoritos(id){
+    //fetch para buscar cada elemento del array. 
+    let url = `https://api.themoviedb.org/3/movie/popular${storage[i].id}?api_key=3e29fc479dc4e2ffecc7ae48fa6e551d&language=en-US&page=1`
+
+    fetch(url)
+        .then( function(response){
+            return response.json();
+        })
+        .then( function(data){
+            console.log(data);
+            personajesFavoritos +=  `<article class="art-peli">
+            <a class="peli" href="./detail-movies.html${info[i].id}" > 
+                <img src=https://image.tmdb.org/t/p/w154/${info[i].poster_path}>
+                <p class="titulopeli">${info[i].title}</p>
+                <p class="fecha">${info[i].release_date}</p>
+            </a>
+        </article>`
+        
+            //mostarlo al usuario.
+            section.innerHTML = personajesFavoritos;            
+        })
+        .catch( function(error){
+            console.log(error);
+        })
+
+}
 
 
 /* //Series favoritos
