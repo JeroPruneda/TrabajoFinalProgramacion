28b05c8079437f56c09f68e176781a9c981bb83c
BrunogGallo <brunogiorgiogallo@gmail.com>
2021-11-23 13:06:19 -0300

    corrijo errores menores

--- clase-g-git/js/detail-genres.js
+++ clase-g-git/js/detail-genres.js
@@ -20,6 +20,8 @@ formulario.addEventListener ('submit', function (event) {
     var id = query2.get("id")
     let name = query2.get("name")
     let categoria = query2.get("categoria")
+    let tituloPagina = document.querySelector ('title')
+
     if (categoria == "pelicula") {
 
         fetch(`https://api.themoviedb.org/3/discover/movie?api_key=38d8aa65e8dac91d5716d80acccd64eb&language=en-US&sort_by=popularity.desc&with_genres=${id}`)
@@ -31,10 +33,12 @@ formulario.addEventListener ('submit', function (event) {
         .then(function(datos){
             console.log(datos);
           let articlegeneros = document.querySelector('.articlegeneros')
+
           articlegeneros.innerHTML += `<h2>Usted esta viendo peliculas del genero ${name}</h2>`
+          tituloPagina.innerText = `${name} - Moovify`
         let peliculas = document.querySelector('.peliculas')
         datos.results.forEach(datos => {peliculas.innerHTML +=` <article class="art-pelicula">
-        <a href="../html/detail-movies.html" class="pelicula"><img src="https://image.tmdb.org/t/p/w500/${datos.poster_path}" >
+        <a href="../html/detail-movies.html?id=${datos.id}" class="pelicula"><img src="https://image.tmdb.org/t/p/w500/${datos.poster_path}" >
         <p class="titulopelicula">${datos.title}</p> 
         </a>
         <br>
@@ -60,9 +64,10 @@ formulario.addEventListener ('submit', function (event) {
             console.log(datos);
           let articlegeneros = document.querySelector('.articlegeneros')
           articlegeneros.innerHTML += `<h2>Usted esta viendo series del genero ${name}</h2>`
+          tituloPagina.innerText = `${name} - Moovify`
         let peliculas = document.querySelector('.peliculas')
         datos.results.forEach(datos => {peliculas.innerHTML +=` <article class="art-pelicula">
-        <a href="../html/detail-movies.html" class="pelicula"><img src="https://image.tmdb.org/t/p/w500/${datos.poster_path}" >
+        <a href="../html/detail-series.html?id=${datos.id}" class="pelicula"><img src="https://image.tmdb.org/t/p/w500/${datos.poster_path}" >
         <p class="titulopelicula">${datos.name}</p> 
         </a>
         <br>
